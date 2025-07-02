4a6b5baca1937af46fae2e8c6730ae1f84066f61
shiv098765 <borseyogesh144@gmail.com>
2025-07-02 23:17:36 +0530

    Add files via upload

---
+++ chatbox.py
@@ -0,0 +1,36 @@
+import streamlit as st
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.messages import HumanMessage
+from dotenv import load_dotenv
+import os
+
+
+load_dotenv()
+
+
+
+st.title("chat box")
+st.write("Gemini 1.5 Flash model")
+
+prompt = st.text_input("Enter your question:")
+
+
+if st.button("Get Answer"):
+    if prompt:
+        try:
+            
+            model = ChatGoogleGenerativeAI(model="gemini-1.5-flash")
+
+          
+            response = model.invoke([
+                HumanMessage(content=prompt)
+            ])
+
+           
+            st.subheader("Gemini's Answer:")
+            st.write(response.content)
+
+        except Exception as e:
+            st.error(f" Error: {e}")
+    else:
+        st.warning(" Please enter a question.")
